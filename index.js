8a693079097463caca1ee0cd7dcfd6dfcd79c886
petarchord <petarchord@hotmail.com>
2021-10-10 01:23:06 +0200

    updated CORS

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const cors = require("cors");
 const app = express();
 
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: "https://fairshost-fe.herokuapp.com/",
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
   credentials: true,
 };
